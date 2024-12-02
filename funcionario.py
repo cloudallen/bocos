b7c25387a62ae6269f0a2dc265a2902d5857197c
David Junior <cloudallen.melo@gmail.com>
2024-12-02 12:35:45 -0300

    Add files via upload

--- funcionario.py
+++ funcionario.py
@@ -1,4 +1,3 @@
-
 def primos_ate(n):
     e = eratosteles(n)
     return [e[i] for i in range(n) if e[i]!=False]
@@ -9,6 +8,14 @@ def maior_fator_primo_de(n):
         if n % p==0:
             return p
     return n
+
+def e_primo(n):
+    if i==1:
+        return False
+    for i in range(2,n**0.5,1):
+        if n%i==0:
+            return False
+    return True
 
 def eratosteles(n):
     flags = list(range(n+1))
